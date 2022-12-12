70ca8c536f1cc1723444a35650a5a5e49b30240b
kateka salman <120364607+quereshisalman143@users.noreply.github.com>
2022-12-12 14:17:33 +0530

    Create prgm1.py

---
+++ prgm1.py
@@ -0,0 +1,14 @@
+def list_occurrences(l):
+    return True if l.count(19) == 2 and l.count(5) >= 3 else False
+
+print(list_occurrences([19, 19, 15, 5, 3, 5, 5, 2]))
+
+def list_occurrences(l):
+    return True if l.count(19) == 2 and l.count(5) >= 3 else False
+
+print(list_occurrences([19, 15, 15, 5, 3, 3, 5, 2]))
+
+def list_occurrences(l):
+    return True if l.count(19) == 2 and l.count(5) >= 3 else False
+
+print(list_occurrences([19, 19, 5, 5, 5, 5, 5]))
